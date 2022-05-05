f2dac7294529ce59049d66e641bb8b9877833d39
dev-nayandas <rich.nayan@gmail.com>
2022-05-05 15:29:30 +0600

    few issue solved

--- src/Pages/Home/Customer/Customer.js
+++ src/Pages/Home/Customer/Customer.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Customer = () => {
     return (
         <div className='mb-3 mt-5'>
-            <h3 className='mt-5'>We have partnered with the best shipping carriers around the world.</h3>
+            <h3 className='mt-5'>We have clients with the best Gym equipemnt around the world.</h3>
             <div  className='container mt-3 mb-5'>
                 <div className='row'>
                     <img style={{width:'100px', height: '80px'}} src='https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
