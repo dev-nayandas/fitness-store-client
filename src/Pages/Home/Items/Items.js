8642b1521807c2a05044d97789151ac3ed88dd35
dev-nayandas <rich.nayan@gmail.com>
2022-05-05 15:23:31 +0600

    Items section done

--- src/Pages/Home/Items/Items.js
+++ src/Pages/Home/Items/Items.js
@@ -7,34 +7,48 @@ import { Table } from 'react-bootstrap';
 const Items = () => {
     return (
         <div className='mx-auto mt-5 mb-5' style={{height:'300px', width:'800px'}}>
-            <h2>The Items that we have now</h2>
+            <h2>Most popular Items that we have now!</h2>
             <Table striped bordered hover variant="dark">
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>Sl No</th>
+      <th>Eqipment's Name</th>
+      <th>Brand</th>
+      <th>Qunatity</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>Treadmil</td>
+      <td>Life Fitness</td>
+      <td>256 Pcs</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Elliptical Machine</td>
+      <td>Skybex</td>
+      <td>452 Pcs</td>
     </tr>
     <tr>
       <td>3</td>
-      <td colSpan={2}>Larry the Bird</td>
-      <td>@twitter</td>
+      <td>Indoor Exercise Spin Bike</td>
+      <td>Star Trac</td>
+      <td>273 Pcs</td>
     </tr>
+    <tr>
+      <td>4</td>
+      <td>Mini Exercise Bike</td>
+      <td>Stair Master</td>
+      <td>456 Pcs</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Rowing Machinergery</td>
+      <td>Hamer Strength</td>
+      <td>159 Pcs</td>
+    </tr>
+    
   </tbody>
 </Table>
         </div>
