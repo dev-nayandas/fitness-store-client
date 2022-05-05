6334f53ccd47323ef1c22f41ffeb3dfadfb3a832
dev-nayandas <rich.nayan@gmail.com>
2022-05-05 15:11:54 +0600

    error page ui improved

--- src/Pages/ErrorPage/ErronPage.js
+++ src/Pages/ErrorPage/ErronPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 const ErronPage = () => {
     return (
         <div>
-            <h1 className='text-bold'>The page you are looking for are not found</h1>
+            <h1 className='text-bold mt-5 '>The page you are looking for are not found</h1>
+            <h1>Error 404</h1>
+            <img className='w-100' src="https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
         </div>
     );
 };
